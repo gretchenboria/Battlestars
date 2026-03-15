fdfb09c957b03f4f72357ebfbbe2d848ed34201d
Jorge A Jimenez <me@jorgeajimenez.com>
2026-03-14 17:32:17 -0700

    Fix remote git pull by removing logs and force resetting

--- submit_all_to_popcorn.sh
+++ submit_all_to_popcorn.sh
@@ -10,7 +10,7 @@ fi
 
 echo "Starting remote submission for all problems..."
 
-COMMAND="export PATH=\"\$HOME/.local/bin:\$PATH\" && cd ~/Battlestars && git pull origin hackathon && cd helion && source ~/helion_env/bin/activate && export HELION_AUTOTUNE_PRECOMPILE=spawn && \
+export COMMAND="export PATH=\"\$HOME/.local/bin:\$PATH\" && cd ~/Battlestars && rm -rf helion/logs helion/eval.log && git fetch origin hackathon && git reset --hard origin/hackathon && cd helion && source ~/helion_env/bin/activate && export HELION_AUTOTUNE_PRECOMPILE=spawn && \
 cd causal_conv1d_py && echo 'Submitting causal_conv1d_py' && popcorn submit --mode leaderboard submission.py && cd .. && \
 cd fp8_quant_py && echo 'Submitting fp8_quant_py' && popcorn submit --mode leaderboard submission.py && cd .. && \
 cd gated_deltanet_chunk_fwd_h_py && echo 'Submitting gated_deltanet_chunk_fwd_h_py' && popcorn submit --mode leaderboard submission.py && cd .. && \
@@ -19,7 +19,7 @@ cd gated_deltanet_recompute_w_u_py && echo 'Submitting gated_deltanet_recompute_
 
 /usr/bin/expect -c '
 set timeout -1
-spawn ssh -o StrictHostKeyChecking=accept-new "$env(username)@$env(host)" '"\"$COMMAND\""'
+spawn ssh -o StrictHostKeyChecking=accept-new "$env(username)@$env(host)" "$env(COMMAND)"
 expect "*assword:*"
 send "$env(password)\r"
 expect eof
